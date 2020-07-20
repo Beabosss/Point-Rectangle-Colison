1684e3a8fee8ef8a4c56b37f490b6b643a8753e8
Rithwik Gupta <rithwikgupta@rithwiks-air.lan>
2020-07-20 10:05:10 -0700

    Made It scale With Screen Size and the Rectangle is at the center of the screen

--- Build Directory/sketch.js
+++ Build Directory/sketch.js
@@ -1,6 +1,9 @@
 function setup() {
-  createCanvas(700, 700);
+  createCanvas(window.innerWidth, window.innerHeight);
 }
+
+
+window.addEventListener("resize", setup);
 
 function collidePointRect(px, py, rx1, ry1, rx2, ry2){
   if (px >= rx1 && px <= rx1 + Math.abs(rx1 - rx2) && py >= ry1 && py <= ry1 + Math.abs(ry1 - ry2)){
@@ -17,10 +20,10 @@ function draw() {
   background(220);
   let px = mouseX
   let py = mouseY
-  let rx1 = 350
-  let ry1 = 350
-  let rx2 = 500
-  let ry2 = 500
+  let rx1 = window.innerWidth/2 - 75
+  let ry1 = window.innerHeight/2 - 100
+  let rx2 = window.innerWidth/2 + 75
+  let ry2 = window.innerHeight/2 + 100
   
   rect(rx1, ry1, Math.abs(rx1 - rx2), Math.abs(ry1 - ry2))
   
